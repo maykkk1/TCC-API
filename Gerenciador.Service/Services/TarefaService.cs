3075bab12ffa2852d60ff157adf525b0b9d06d9e
Maycon <maycon5591@gmail.com>
2024-03-09 23:50:26 -0300

    Ajuste na logica de update de tarefa

--- Gerenciador.Service/Services/TarefaService.cs
+++ Gerenciador.Service/Services/TarefaService.cs
@@ -92,61 +92,11 @@ public class TarefaService : ITarefaService
     
     public async Task<ServiceResult<TarefaDto>> UpdateTarefaPrincipal(Tarefa tarefa, int userId)
     {
-        var result = new ServiceResult<TarefaDto>();
-        var oldTarefa = await _dbContext.Set<Tarefa>().AsNoTracking().Where(t => t.Id == tarefa.Id).FirstOrDefaultAsync();
-        var tarefaOrientador = await _dbContext.Set<Tarefa>().AsNoTracking().Where(t => t.IdTarefaRelacionada == oldTarefa.Id).FirstOrDefaultAsync();
-
-        if (tarefaOrientador != null)
-        {
-            // verificar a logica da tarefa estar indo para analise
-            if (tarefaOrientador.Situacao != SituacaoTarefaEnum.Pendente)
-            {
-                result.Success = false;
-                result.ErrorMessage = "Você não pode modificar a situação dessa tarefa pois o orientador já iniciou o processo de avaliação.";
-                return result;
-            }
-        }
-
-        if (oldTarefa == null)
-        {
-            result.Success = false;
-            result.ErrorMessage = "Tarefa não encontrada.";
-            return result;
-        }
-
-        if (oldTarefa.TarefaRelacionada != null && oldTarefa.TarefaRelacionada.Situacao != SituacaoTarefaEnum.Pendente)
-        {
-            result.Success = false;
-            result.ErrorMessage = "Você não pode modificar o status dessa tarefa pois o orientador já começou o processo de análise dela.";
-            return result;
-        }
-
-        if (tarefaOrientador != null && oldTarefa.Situacao == SituacaoTarefaEnum.Analise)
-        {
-            await _tarefaRepository.Delete(tarefaOrientador.Id);
-        }
-        
         await _tarefaRepository.Update(tarefa);
-        if (tarefa.Situacao == SituacaoTarefaEnum.Analise)
+        var dto = _tarefaMapper.ToDto(tarefa);
+        return new ServiceResult<TarefaDto>()
         {
-            var orientando = _dbContext.Set<User>().Where(u => u.Id == userId).Include(u => u.Orientador).FirstOrDefault();
-            var newTarefaOrientador = new Tarefa()
-            {
-                Titulo = "Tarefa criada depois que aluno pede analise",
-                Descricao = "Tarefa criada depois que aluno pede analise",
-                Tipo = TipoTarefa.Principal,
-                Situacao = SituacaoTarefaEnum.Pendente,
-                CreatedById = orientando.Id,
-                IdPessoa = orientando.Orientador.Id,
-                DataCriacao = DateTime.Now,
-                IdTarefaRelacionada = tarefa.Id
-            };
-            await InsertTarefaPrincipal(newTarefaOrientador);
-        }
-
-        result.Success = true;
-        //ajustar isso aqui
-        result.Data = new TarefaDto();
-        return result;
+            Data = dto
+        };
     }
 }
